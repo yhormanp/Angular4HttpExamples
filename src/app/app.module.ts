778078f35e236abf12b010a6295dff59b91e330a
Yhorman Perez <jhppunk@gmail.com>
2018-02-22 12:09:12 -0500

    example of http from  ngbook2angular4 page 237

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { SimpleHttpComponent } from './simple-http/simple-http.component'
 import { AppComponent } from './app.component';
+import { YouTubeSearchComponent } from './you-tube-search/you-tube-search.component';
+import { youTubeSearchInjectables } from './you-tube-search/you-tube-search.injectable';
+import { SearchResultComponent } from './you-tube-search/search-result.component';
+import { SearchBoxComponent } from './you-tube-search/search-box.component';
+import { MoreHttpRequestsComponent } from './more-http-requests/more-http-requests.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleHttpComponent,
+    YouTubeSearchComponent,
+    SearchResultComponent,
+    SearchBoxComponent,
+    MoreHttpRequestsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [youTubeSearchInjectables],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
